1b8bee8b8dd9ecb02f4e733e14a52ae897f4576d
Petter Svensson <petter.svensson.90@gmail.com>
2019-09-03 15:03:40 +0200

    Fire mat, incinerator script, petter scene

---
+++ Assets/Scripts/Interactables/Incinerator.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Incinerator : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Interactable"))
+        {
+            Debug.Log("Yay");
+            Destroy(other.gameObject);
+        }
+    }
+}
