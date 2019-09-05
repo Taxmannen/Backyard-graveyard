32f22ed1d6b5d098dfe5bf0587d00645cc7aefb5
Petter Svensson <petter.svensson.90@gmail.com>
2019-09-05 11:49:32 +0200

    My scene, added functionality for body parts

--- Assets/Scripts/Body.cs
+++ Assets/Scripts/Body.cs
@@ -7,16 +7,58 @@ public class Body : Pickup
 {
     [SerializeField] private Transform headPosition;
 
-    public bool hasHead = false;
+    [SerializeField] private FixedJoint fixedJoint;
+
+    public bool fullBody;
+    
+    public enum MyColor {Blue, Green, Red};
+
+    private void Awake()
+    {
+        MyColor myColor = (MyColor)Random.Range(0, 3);
+        if (myColor == MyColor.Blue)
+        {
+            gameObject.GetComponent<MeshRenderer>().material.color = Color.blue;
+        }
+        else if (myColor == MyColor.Green)
+        {
+            gameObject.GetComponent<MeshRenderer>().material.color = Color.green;
+        }
+        else if (myColor == MyColor.Red)
+        {
+            gameObject.GetComponent<MeshRenderer>().material.color = Color.red;
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.GetComponent<Head>() && !hasHead)
+        if (other.GetComponent<Head>())
         {
             other.gameObject.transform.position = headPosition.position;
-            gameObject.GetComponent<SpringJoint>().connectedBody = other.gameObject.GetComponent<Rigidbody>();
-            hasHead = true;
-            gameObject.AddComponent<FullBody>();
+            other.gameObject.transform.rotation = Quaternion.Euler(headPosition.eulerAngles);
+            //other.gameObject.GetComponent<Rigidbody>().useGravity = false;
+            fullBody = true;
+            if (fixedJoint != null)
+            {
+                Destroy(fixedJoint);
+                fixedJoint = null;
+            }
+            fixedJoint = gameObject.AddComponent<FixedJoint>();
+            fixedJoint.connectedBody = other.gameObject.GetComponent<Rigidbody>();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.GetComponent<Head>())
+        {
+            if (fixedJoint != null)
+            {
+                Destroy(gameObject.GetComponent<FixedJoint>());
+                fixedJoint = null;
+            }
+            //other.gameObject.GetComponent<Rigidbody>().useGravity = true;
+            fullBody = false;
         }
     }
 }
--- Assets/Scripts/Head.cs
+++ Assets/Scripts/Head.cs
@@ -2,17 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Head : MonoBehaviour
+public class Head : Pickup
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public enum MyColor { Blue, Green, Red };
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        MyColor myColor = (MyColor)Random.Range(0, 3);
+
+        MeshRenderer[] meshRenderer = GetComponentsInChildren<MeshRenderer>();
+
+        if (myColor == MyColor.Blue)
+        {
+            for (int i = 0; i < meshRenderer.Length; i++)
+            {
+                meshRenderer[i].material.color = Color.blue;
+            }
+        }
+        else if (myColor == MyColor.Green)
+        {
+            for (int i = 0; i < meshRenderer.Length; i++)
+            {
+                meshRenderer[i].material.color = Color.green;
+            }
+        }
+        else if (myColor == MyColor.Red)
+        {
+            for (int i = 0; i < meshRenderer.Length; i++)
+            {
+                meshRenderer[i].material.color = Color.red;
+            }
+        }
     }
 }
