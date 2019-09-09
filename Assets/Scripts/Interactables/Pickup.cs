87f8d25906877783b0e03c5415083ad5d4804a62
Daniel Nordahl <danielnordahl93@gmail.com>
2019-09-09 12:53:05 +0200

    Fixed Highlight Bug, DespawnOnGround Bug and Sword Position

--- Assets/Scripts/Interactables/Pickup.cs
+++ Assets/Scripts/Interactables/Pickup.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 /* Script Made By Daniel */
 [RequireComponent(typeof(Rigidbody))]
@@ -10,10 +11,22 @@ public class Pickup : Interactable
     [SerializeField] protected bool shouldDespawnWhenOnGround;
     [SerializeField] protected float despawnTimeWhenOnGround;
 
+    private Coroutine coroutine;
+
     public bool SnapOnPickup
     {
         get { return snapOnPickup; }
         set { snapOnPickup = value; }
+    }
+
+    public override Interactable Interact()
+    {
+        if (coroutine != null)
+        {
+            StopCoroutine(coroutine);
+            coroutine = null;
+        }
+        return this;
     }
 
     public virtual void Drop()
@@ -26,12 +39,19 @@ public class Pickup : Interactable
     {
         if (other.gameObject.tag == "Ground" && ActiveHand == null && shouldDespawnWhenOnGround)
         {
-            Destroy(gameObject, despawnTimeWhenOnGround);
+            if (coroutine == null) coroutine = StartCoroutine(DestoryMe());
+            //Destroy(gameObject, despawnTimeWhenOnGround);
         }
     }
 
     private void OnDestroy()
     {
         if (ActiveHand != null) ActiveHand.Drop();
+    }
+
+    private IEnumerator DestoryMe()
+    {
+        yield return new WaitForSeconds(despawnTimeWhenOnGround);
+        Destroy(gameObject);
     }
 }
