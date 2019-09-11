245e4d498fe31cde78e30cdc9b95e6f9dd6b0682
Daniel Nordahl <danielnordahl93@gmail.com>
2019-09-11 14:16:26 +0200

    Added FindMe Script

---
+++ Assets/Scripts/Debug/FindMe.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+/* Script Made By Daniel */
+public class FindMe : MonoBehaviour
+{
+    [SerializeField] private string scriptName = "Interactable";
+
+    private void Awake()
+    {
+        Debug.Log("FindMe: " + gameObject.name);
+        if (scriptName.Length > 0)
+        {
+            Type type = Type.GetType(scriptName);
+            UnityEngine.Object[] objects = FindObjectsOfType(type);
+            for (int i = 0; i < objects.Length; i++) Debug.Log(objects[i].name);
+        }
+    }
+}
