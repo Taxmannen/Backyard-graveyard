63d063f9a855119cc2b5ac067f483dff569e1275
Daniel Nordahl <danielnordahl93@gmail.com>
2019-09-16 15:36:14 +0200

    Fixed ActiveHand Bug

--- Assets/Scripts/Interactables/Pickup/Treatment.cs
+++ Assets/Scripts/Interactables/Pickup/Treatment.cs
@@ -1,16 +1,5 @@
 ﻿using UnityEngine;
 
-/*
-    What it’s used for: Treatments are parts of Tasks. Every body will require exactly one treatment. (Might want to lower the requirement to zero treatments, but never more).
-    How it works
-    Once a treatment is applied to a body, all previously performed treatments on that body are cleared. 
-
-    Type of Treatments:
-    Make-up - Paint objects and environment with a lipstick. One dot of lipstick on a body part is enough to fulfill the task. “Unity Decal” for applying the make-up?
-    Wash-up - Clean the deceased with a sponge. One touch on a body part is enough to fulfill the task. Particle system with sparkles whenever an object is sponged?
-    Mummify - Cover the body in mummy-cloth. Can we add a mummy-material to any object that the cloth is applied to?
- */
-
 public enum TreatmentType { MakeUp, WashUp, Mummify, None, [System.ObsoleteAttribute] NumberOfTypes }
 
 /* Script Made By Daniel */
@@ -27,7 +16,7 @@ public class Treatment : Pickup
         if (other.CompareTag("Interactable"))
         {
             BodyPart bodyPart = other.GetComponent<BodyPart>();
-            if (bodyPart) bodyPart.SetTreatment(treatmentType);
+            if (bodyPart && bodyPart.ActiveHand) bodyPart.SetTreatment(treatmentType);
         }
     }
 }
