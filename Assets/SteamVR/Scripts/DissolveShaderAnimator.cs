631de1ea456a1846895f9e63845a5ad287cfef19
Simon Ektjärn <debarquer@gmail.com>
2019-09-16 16:09:54 +0200

    added stuff and things

---
+++ Assets/SteamVR/Scripts/DissolveShaderAnimator.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DissolveShaderAnimator : MonoBehaviour
+{
+    Material mat;
+    bool dissolve = true;
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.M)) {
+            Debug.Log("Dissolving...");
+
+            if(dissolve)
+                Dissolve(0f, 1f, 1f);
+            else
+                Dissolve(1f, 0f, 1f);
+
+            dissolve = !dissolve;
+        }
+    }
+
+    public void Dissolve(float from, float to, float duration) {
+        mat = GetComponent<Renderer>().material;
+        StartCoroutine(DissolveCoroutine(from, to, duration));
+    }
+
+    private IEnumerator DissolveCoroutine(float from, float to, float duration) {
+        float v;
+
+        try {
+            v = mat.GetFloat("_cutoff");
+        }
+        catch(System.Exception e) {
+            Debug.LogError("DissolveShaderAnimator exception: " + e);
+            yield break;
+        }
+
+        for (float i = 0; i <= 1; i += Time.deltaTime / duration) {
+            mat.SetFloat("_cutoff", Mathf.Lerp(from, to, i));
+            yield return null;
+        }
+        mat.SetFloat("_cutoff", to);
+    }
+}
