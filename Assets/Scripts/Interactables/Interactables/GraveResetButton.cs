efe74cd9ad4142aff40c3211a115d85e1f01e73d
Kristoffer Svedlund <kristoffersvedlund@gmail.com>
2019-09-17 14:19:35 +0200

    UnrestMeter graphics and functionality

--- Assets/Scripts/Interactables/Interactables/GraveResetButton.cs
+++ Assets/Scripts/Interactables/Interactables/GraveResetButton.cs
@@ -6,7 +6,7 @@ public class GraveResetButton : Interactable
 {
 
     private Grave grave;
-    private float cooldownTime = 3f;
+    private float cooldownTime = 2f;
     private bool onCooldown;
 
     private void Awake()
@@ -27,6 +27,7 @@ public class GraveResetButton : Interactable
 
     private IEnumerator Cooldown()
     {
+        onCooldown = true;
         yield return new WaitForSeconds(cooldownTime);
         onCooldown = false;
         gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + 0.05f, gameObject.transform.position.z);
---
+++ Assets/UnrestMeter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UnrestMeter : MonoBehaviour
+{
+    [SerializeField] private Sprite neutralSprite;
+    [SerializeField] private Sprite happySprite;
+    [SerializeField] private Sprite angrySprite;
+
+    [SerializeField] private Image ghostFaceImage;
+    [SerializeField] private Slider meterValue;
+
+    private int unrestValue;
+
+
+    public void UpdateUnrestMeter(int unrest)
+    {
+        unrestValue = unrest;
+        SetMeterValue();
+    }
+
+    private void SetMeterValue()
+    {
+        SetCorrectSprite();
+        SetSpritePosition();
+    }
+
+    private void SetCorrectSprite()
+    {
+        if (unrestValue < 6) ghostFaceImage.sprite = happySprite;
+        else if (unrestValue > 14) ghostFaceImage.sprite = angrySprite;
+        else ghostFaceImage.sprite = neutralSprite;
+    }
+
+    private void SetSpritePosition()
+    {
+        meterValue.value = unrestValue;
+    }
+}
