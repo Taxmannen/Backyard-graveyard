a8f80e347fcc5d448711a69a192724a5f03d6916
Kristoffer Svedlund <kristoffersvedlund@gmail.com>
2019-09-18 11:39:28 +0200

    Torches and Lanterns implemented. Torches will need a spawner which lights the torch when picked up, Lantern needs proper Unrest-event functionality.

---
+++ Assets/Lantern.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lantern : MonoBehaviour
+{
+    private FireHolder fireHolder;
+    private float secondsThatTheLanternHasBeenLit;
+
+    [SerializeField] private float secondsLitForUnrestDecrease;
+
+    private void Start()
+    {
+        fireHolder = GetComponentInChildren<FireHolder>();
+    }
+
+    private void Update()
+    {
+        if (fireHolder.isLit)
+        {
+            secondsThatTheLanternHasBeenLit += Time.deltaTime;
+            if (secondsThatTheLanternHasBeenLit >= secondsLitForUnrestDecrease)
+            {
+                secondsThatTheLanternHasBeenLit = 0f;
+                Debug.Log("Unrest goes down by one");
+            }
+        }
+        else
+        {
+            secondsThatTheLanternHasBeenLit = 0f;
+        }
+    }
+}
