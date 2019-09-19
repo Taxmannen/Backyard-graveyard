e5349f50cf347dd6be7643cc289d37eaa1f84017
Petter Svensson <petter.svensson.90@gmail.com>
2019-09-19 15:53:48 +0200

    improved collision(NOT DONE!)

---
+++ Assets/Scripts/Game/CollisionManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+/* Script Made By Daniel & Petter */
+public class CollisionManager : Singleton<CollisionManager>
+{
+    [SerializeField] private bool collisionTest = false;
+    [SerializeField] private Collider[] staticColliders;
+
+    public void SetColliderState(Collider[] colliders, bool state)
+    {
+        foreach (Collider collider in staticColliders)
+        {
+            foreach (Collider pickupCollider in colliders) Physics.IgnoreCollision(collider, pickupCollider, state);
+        }
+    }
+
+    public bool GetCollisionTest() { return collisionTest; }
+}
