9b8da01f4318984614b25905682ad6eed9fd1f42
Kristoffer Svedlund <kristoffersvedlund@gmail.com>
2019-09-26 13:20:02 +0200

    Very basic AudioManager for handling specifik sfx

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioClip despawnSound;
+
+    public void PlaySoundAtPosition(AudioClip clip, Transform sourceTransform)
+    {
+        AudioSource.PlayClipAtPoint(clip, sourceTransform.position);
+    }
+}
