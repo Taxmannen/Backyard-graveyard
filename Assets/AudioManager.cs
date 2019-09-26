921b21df746dbf3650af5b74fb0518ba0f27b0d9
Kristoffer Svedlund <kristoffersvedlund@gmail.com>
2019-09-26 13:24:25 +0200

    AduioManager Singleton

--- Assets/AudioManager.cs
+++ Assets/AudioManager.cs
@@ -2,9 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AudioManager : MonoBehaviour
+public class AudioManager : Singleton<AudioManager>
 {
     public AudioClip despawnSound;
+
+    private void Awake()
+    {
+        SetInstance(this);
+    }
 
     public void PlaySoundAtPosition(AudioClip clip, Transform sourceTransform)
     {
